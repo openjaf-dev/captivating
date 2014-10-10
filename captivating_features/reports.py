f65c72d347039d8ebf599d9408c50433d4932c65
Yuri Quintana <yuriq@uh.cu>
2014-10-10 22:39:51 +0200

    Optimizando los campos funcionales relacionados con los reportes

--- captivating_features/reports.py
+++ captivating_features/reports.py
@@ -30,13 +30,22 @@ class sale_order_line(Model):
     _name = 'sale.order.line'
     _inherit = 'sale.order.line'
     
+    def _get_related_order_lines(self, cr, uid, ids, context=None):
+        # Get all order_lines belonging to same sale_order 
+        res = {}
+        for ol1 in self.browse(cr, uid, ids, context=context):
+            for ol2 in ol1.order_id.order_line:
+                res[ol2.id] = True
+                
+        return res.keys()  
+        
     def _get_hotel_from(self, cr, uid, ids, fields, args, context=None):        
         return self.get_hotel_match(cr, uid, ids, 'end_date', context)
         
     def _get_hotel_to(self, cr, uid, ids, fields, args, context=None):          
         return self.get_hotel_match(cr, uid, ids, 'start_date', context)
     
-    def get_hotel_match(self, cr, uid, ids, target_date, context):
+    def _compute_start_end(self, cr, uid, ids, fields, args, context=None):
         
         result = {} 
           
@@ -45,30 +54,47 @@ class sale_order_line(Model):
         order_line = self.pool.get('sale.order.line')
         
         for obj in self.browse(cr, uid, ids, context):
-            result[obj.id] = ''
             if obj.product_id.categ_id.name == 'Transfer':
                 sale_obj = sale.browse(cr, uid, obj.order_id.id)
                 for order_line_obj in sale_obj.order_line:
                     product = order_line_obj.product_id
-                    if product.categ_id.name == 'Hotel' and getattr(order_line_obj, target_date) == obj.start_date:
-                        result[obj.id] = product.name      
+                    if product.categ_id.name == 'Hotel':
+                        if order_line_obj.end_date == obj.start_date:
+                            result.setdefault(obj.id, {'hotel_from': product.name, 'hotel_to': ''})
+                            result[obj.id]['hotel_from'] = product.name   
+                        elif order_line_obj.start_date == obj.start_date:
+                            result.setdefault(obj.id, {'hotel_from': '', 'hotel_to': product.name})
+                            result[obj.id]['hotel_to'] = product.name     
           
         return result    
     
     _columns = {             
         'hotel_from':
-            fields.function(_get_hotel_from, method=True, type='char',
-                            string='Hotel from', size=128),             
+            fields.function(_compute_start_end, method=True, type='char',
+                            string='Hotel from', size=128,
+                            store = {'sale.order.line': (_get_related_order_lines, ['start_date', 'end_date'], 10)},
+                            multi = 'from_to'),             
         'hotel_to':
-            fields.function(_get_hotel_to, method=True, type='char',
-                            string='Hotel to', size=128),
+            fields.function(_compute_start_end, method=True, type='char',
+                            string='Hotel to', size=128,
+                            store = {'sale.order.line': (_get_related_order_lines, ['start_date', 'end_date'], 10)},
+                            multi = 'from_to')
                    
         
     }
     
 class sale_order(Model):
     _name = 'sale.order'
-    _inherit = 'sale.order'
+    _inherit = 'sale.order'   
+     
+    def _get_related_orders(self, cr, uid, ids, context=None):
+        # Get all order_lines belonging to same sale_order 
+        res = {}
+        order_lines = self.pool.get('sale.order.line').browse(cr, uid, ids, context=context)
+        for ol in order_lines:
+                res[ol.order_id.id] = True
+                
+        return res.keys()  
     
     def _get_start_transfer(self, cr, uid, ids, fields, args, context=None):        
         return self.get_product_match(cr, uid, ids, 'start_date', 'date_order', 'Transfer', context)
@@ -82,30 +108,85 @@ class sale_order(Model):
     def _get_end_hotel(self, cr, uid, ids, fields, args, context=None):          
         return self.get_product_match(cr, uid, ids, 'end_date', 'end_date', 'Hotel', context)
          
-    def get_product_match(self, cr, uid, ids, order_line_date, order_date, product_name, context):        
+    def _compute_start_end(self, cr, uid, ids, fields, args, context=None):        
         result = {} 
                 
-        for obj in self.browse(cr, uid, ids, context):
-            result[obj.id] = ''
-            for order_line_obj in obj.order_line:
-                product = order_line_obj.product_id
-                if product.categ_id.name == product_name and getattr(order_line_obj, order_line_date) == getattr(obj, order_date):
-                    result[obj.id] = product.name + " (" + str(order_line_obj.reservation_number) + ")"      
-                  
-        return result       
-    
+        for order_obj in self.browse(cr, uid, ids, context):
+            order_lines = order_obj.order_line
+            for i in range(0, len(order_obj.order_line)-1):
+                for j in range(i+1, len(order_obj.order_line)):
+                    hotel = None
+                    transfer = None
+                    def get_product(product_name):
+                        for idx in [i,j]:
+                            if order_lines[idx].product_id.categ_id.name == product_name:
+                                return order_lines[idx]
+                        return None
+                    hotel = get_product('Hotel')
+                    transfer = get_product('Transfer')
+                    if hotel and transfer:
+                        transfer_str = transfer.name + " (" + str(transfer.reservation_number) + ")"
+                        hotel_str = hotel.name + " (" + str(hotel.reservation_number) + ")"
+                        if order_obj.date_order == transfer.start_date:
+                            result.setdefault(order_obj.id, {'start_transfer': transfer_str, 
+                                                       'end_transfer': '',
+                                                       'start_hotel': '',
+                                                       'end_hotel': ''})
+                            result[order_obj.id]['start_transfer'] = transfer_str
+                        if order_obj.date_order == hotel.start_date:  
+                            result.setdefault(order_obj.id, {'start_transfer': '', 
+                                                       'end_transfer': '',
+                                                       'start_hotel': hotel_str,
+                                                       'end_hotel': ''})  
+                            result[order_obj.id]['start_hotel'] = hotel_str                        
+                        if order_obj.end_date == transfer.start_date:
+                            result.setdefault(order_obj.id, {'start_transfer': '', 
+                                                       'end_transfer': transfer_str,
+                                                       'start_hotel': '',
+                                                       'end_hotel': ''})
+                            result[order_obj.id]['end_transfer'] = transfer_str
+                        if order_obj.end_date == hotel.end_date:  
+                            result.setdefault(order_obj.id, {'start_transfer': '', 
+                                                       'end_transfer': '',
+                                                       'start_hotel': '',
+                                                       'end_hotel': hotel_str})   
+                            result[order_obj.id]['end_hotel'] = hotel_str
+                            
+        return result   
+#            for order_line_obj in obj.order_line:
+#                product = order_line_obj.product_id
+#                #if product.categ_id.name == product_name and getattr(order_line_obj, order_line_date) == getattr(obj, order_date):
+#                #    result[obj.id] = product.name + " (" + str(order_line_obj.reservation_number) + ")"
+#                if product.categ_id.name == 'Hotel':
+#                    if order_line_obj.end_date == obj.start_date:
+#                        result.setdefault(obj.id, {'hotel_from': product.name, 'hotel_to': ''})
+#                        result[obj.id]['hotel_from'] = product.name   
+#                    elif order_line_obj.start_date == obj.start_date:
+#                        result.setdefault(obj.id, {'hotel_from': '', 'hotel_to': product.name})
+#                        result[obj.id]['hotel_to'] = product.name     
+#                    
+#                elif product.categ_id.name == 'Transfer':   
+                       
     _columns = {             
         'start_transfer':
-            fields.function(_get_start_transfer, method=True, type='char',
-                            string='Start Transfer', size=128),             
+            fields.function(_compute_start_end, method=True, type='char',
+                            string='Start Transfer', size=128,
+                            store = {'sale.order.line': (_get_related_orders, ['start_date', 'end_date'], 10)},
+                            multi = 'from_to'),             
         'end_transfer':
-            fields.function(_get_end_transfer, method=True, type='char',
-                            string='End Transfer', size=128),
+            fields.function(_compute_start_end, method=True, type='char',
+                            string='End Transfer', size=128,
+                            store = {'sale.order.line': (_get_related_orders, ['start_date', 'end_date'], 10)},
+                            multi = 'from_to'),
         'start_hotel':      
-            fields.function(_get_start_hotel, method=True, type='char',
-                            string='Start Hotel', size=128),     
+            fields.function(_compute_start_end, method=True, type='char',
+                            string='Start Hotel', size=128,
+                            store = {'sale.order.line': (_get_related_orders, ['start_date', 'end_date'], 10)},
+                            multi = 'from_to'),     
         'end_hotel':      
-            fields.function(_get_end_hotel, method=True, type='char',
-                            string='End Hotel', size=128),     
+            fields.function(_compute_start_end, method=True, type='char',
+                            string='End Hotel', size=128,
+                            store = {'sale.order.line': (_get_related_orders, ['start_date', 'end_date'], 10)},
+                            multi = 'from_to'),     
         
     }
