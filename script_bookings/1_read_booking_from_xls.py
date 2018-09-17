514730997c25a52c9933c43fe0980b07c127620d
Ariel Torres Gálvez <ariel.freedom@gmail.com>
2018-09-17 10:17:48 -0400

    import booking oct

--- script_bookings/1_read_booking_from_xls.py
+++ script_bookings/1_read_booking_from_xls.py
@@ -4,8 +4,8 @@ import xlrd, datetime, json
 
 BASE_DATE = 693594
 
-file_name = 'files/2018_sept_UK.xls'
-#file_name = 'files/2018_sept_CC.xls'
+file_name = 'files/2018_oct_UK.xls'
+# file_name = 'files/2018_oct_CC.xls'
 
 book = xlrd.open_workbook(file_name, formatting_info=True)
 
