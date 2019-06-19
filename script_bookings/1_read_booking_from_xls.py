ea2788378d64e4b29c6e5c55bbe6fa232a3eb607
atg <ariel.freedom@gmail.com>
2019-06-19 07:08:24 -0400

    import jul 2019

--- script_bookings/1_read_booking_from_xls.py
+++ script_bookings/1_read_booking_from_xls.py
@@ -4,8 +4,8 @@ import xlrd, datetime, json
 
 BASE_DATE = 693594
 
-#file_name = 'files/2019_jun_CC.xls'
-file_name = 'files/2019_jun_UK.xls'
+#file_name = 'files/2019_jul_CC.xls'
+file_name = 'files/2019_jul_UK.xls'
 
 book = xlrd.open_workbook(file_name, formatting_info=True)
 
